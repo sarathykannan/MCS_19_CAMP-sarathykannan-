b6c1a2e96e6628885ba1456ad84ee8fef404b5cb
sarathykannan <50257520+sarathykannan@users.noreply.github.com>
2019-05-09 22:41:35 +0530

    Add files via upload

---
+++ infix.java
@@ -0,0 +1,81 @@
+
+import java.util.Scanner;
+import java.util.Stack; 
+  
+class infix 
+{ 
+   
+    static int Prec(char ch) 
+    { 
+        switch (ch) 
+        { 
+        case '+': 
+        case '-': 
+            return 1; 
+       
+        case '*': 
+        case '/': 
+            return 2; 
+       
+        case '^': 
+            return 3; 
+        } 
+        return -1; 
+    } 
+       
+    
+    static String infixToPostfix(String exp) 
+    { 
+        String result = new String(""); 
+          
+        
+        Stack<Character> stack = new Stack<>(); 
+          
+        for (int i = 0; i<exp.length(); ++i) 
+        { 
+            char c = exp.charAt(i); 
+              
+            
+            if (Character.isLetterOrDigit(c)) 
+                result += c; 
+               
+           
+            else if (c == '(') 
+                stack.push(c); 
+              
+           
+            else if (c == ')') 
+            { 
+                while (!stack.isEmpty() && stack.peek() != '(') 
+                    result += stack.pop(); 
+                  
+                if (!stack.isEmpty() && stack.peek() != '(') 
+                    return "Invalid Expression"; // invalid expression                 
+                else
+                    stack.pop(); 
+            } 
+            else 
+            { 
+                while (!stack.isEmpty() && Prec(c) <= Prec(stack.peek())) 
+                    result += stack.pop(); 
+                stack.push(c); 
+            } 
+       
+        } 
+       
+        
+        while (!stack.isEmpty()) 
+            result += stack.pop(); 
+       
+        return result; 
+    } 
+    
+    
+    public static void main(String[] args)  
+    { Scanner sc=new Scanner(System.in);
+        String exp;
+        exp=sc.next();
+        
+        System.out.println(infixToPostfix(exp)); 
+    }
+} 
